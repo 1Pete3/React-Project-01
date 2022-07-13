6cd287b5b7545c411b3b39cb368581dc92ac10f4
1pete3 <skowronekpeter103@gmail.com>
2022-07-13 18:15:13 -0400

    Project 01 with Scrimba guidance

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+function Navbar() {
+  return (
+    <nav>
+      <img src="logo192.png" alt="React logo" className="reactLogo"></img>
+      <h3 className="ReactFacts">ReactFacts</h3>
+      <h4>React Course - Project 1</h4>
+    </nav>
+  );
+}
+export default Navbar;
